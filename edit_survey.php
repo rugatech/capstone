5b72a6bcd38d7aed07023ced4cbd086f61575d68
rugatech <nick@rugatech.com>
2017-03-25 02:32:43 -0400

    update

--- edit_survey.php
+++ edit_survey.php
@@ -6,7 +6,7 @@ class edit_survey extends template
 	public $questions=[];
 	public $ynRS=['Y'=>'Yes','N'=>'No'];
 	public $qid=[];
-	public $eqRS=['eq'=>'Equals','neg'=>'Not Equals'];
+	public $eqRS=['eq'=>'Equals','neq'=>'Not Equals'];
 	public $hasOptions=[];
 	public $survey=[];
 
@@ -21,17 +21,17 @@ class edit_survey extends template
 		}
 		$pstmt=$this->db->dbh->prepare('SELECT * FROM questions WHERE survey=?');
 		$pstmt->execute([$_GET['token']]);
-		$this->qid['q0']='Always';
+		$this->qid['Y']='Yes, Always';
+		$this->qid['N']='No';
 		while($rs=$pstmt->fetch(PDO::FETCH_ASSOC)){
 			$this->questions[]=$rs;
 			if($this->hasOptions[$rs['question_type']]=='Y'){
-				$this->qid['q'.$rs['pkey']]='If Question (ID='.$rs['pkey'].')';
+				$this->qid['q'.$rs['pkey']]='Yes, If Question (ID='.$rs['pkey'].')';
 			}
 		}
 		$pstmt=$this->db->dbh->prepare('SELECT * FROM surveys WHERE token=? LIMIT 1');
 		$pstmt->execute([$_GET['token']]);
 		$this->survey=$pstmt->fetch(PDO::FETCH_ASSOC);
-		print_r($this->survey);
 	}
 
 	public function showform(){
@@ -66,7 +66,7 @@ class edit_survey extends template
 				<div id="questionsDiv" style="margin-top:20px">
 		  <?php	foreach($this->questions as $key=>$val){ ?>
 					<div class="row">
-						<div class="panel panel-green">
+						<div class="panel panel-green" data-pkey="<?php echo $val['pkey']; ?>" data-has-options="<?php echo $this->hasOptions[$val['question_type']]; ?>">
 							<div class="panel-heading">
 								Question (ID=<?php echo $val['pkey']; ?>)
 								<div class="panel-heading-right">
@@ -77,25 +77,23 @@ class edit_survey extends template
 							<div class="panel-body">
 								<form class="form-horizontal">
 									<div class="form-group">
-										<label class="control-label col-sm-2">Page No:</label>
-										<div class="col-sm-1"><p class="form-control-static"><?php echo $val['page']; ?></p></div>
 										<label class="control-label col-sm-2">Question No:</label>
-										<div class="col-sm-7"><p class="form-control-static"><?php echo $val['question_number']; ?></p></div>
+										<div class="col-sm-10"><p class="form-control-static question-number-p"><?php echo $val['question_number']; ?></p></div>
 									</div>
 									<div class="form-group">
 										<label class="control-label col-sm-2">Question:</label>
-										<div class="col-sm-10"><p class="form-control-static"><?php echo nl2br($val['question']); ?></p></div>
+										<div class="col-sm-10"><p class="form-control-static question-p"><?php echo nl2br($val['question']); ?></p></div>
 									</div>
 									<div class="form-group">
 										<label class="control-label col-sm-2">Type:</label>
-										<div class="col-sm-10"><p class="form-control-static"><?php echo $this->qtypes[$val['question_type']]; ?></p></div>
+										<div class="col-sm-10"><p class="form-control-static question-type-p"><?php echo $this->qtypes[$val['question_type']]; ?></p></div>
 									</div>
 							  <?php	$style='';
 							  		if($this->hasOptions[$val['question_type']]=='N'){$style=' style="display:none"';}; ?>
 									<div class="form-group"<?php echo $style; ?>>
 										<label class="control-label col-sm-2">Options:</label>
 										<div class="col-sm-10">
-											<ul class="list-group">
+											<ul class="list-group options-p" >
 										  <?php	$options=json_decode($val['options'],TRUE);
 												if(empty($options)){echo '<li class="list-group-item empty-list">None</li>';} 
 												else{
@@ -113,22 +111,20 @@ class edit_survey extends template
 								  		if($required[0]=='N'){$style=' style="display:none"';} ?>
 										<label class="control-label col-sm-2" for="question">Required?:</label>
 										<div class="col-sm-10">
-									  <?php	$txt=$this->ynRS[$required[0]];
-									  		if(isset($required[1])){$txt.=', ['.$this->qid[$required[1]].']';}
-									  		if(isset($required[2])){$txt.=' ['.$this->eqRS[$required[2]].']';}
-									  		if(isset($required[3])){$txt.=' ['.$required[3].']';}
-											echo '<p class="form-control-static">'.$txt.'</p>'; ?>
+									  <?php	$txt=$this->qid[$required[0]];
+									  		if(isset($required[1])){$txt.=' ['.$this->eqRS[$required[1]].']';}
+									  		if(isset($required[2])){$txt.=' [Option #'.$required[2].']';}
+											echo '<p class="form-control-static required-p">'.$txt.'</p>'; ?>
 										</div>
 									</div>
 									<div class="form-group">
 								  <?php	$visible=json_decode($val['visible'], TRUE); ?>
 										<label class="control-label col-sm-2" for="question">Visible?:</label>
 										<div class="col-sm-10">
-									  <?php	$txt=$this->ynRS[$visible[0]];
-									  		if(isset($visible[1])){$txt.=', ['.$this->qid[$visible[1]].']';}
-									  		if(isset($visible[2])){$txt.=' ['.$this->eqRS[$visible[2]].']';}
-									  		if(isset($visible[3])){$txt.=' ['.$visible[3].']';}
-											echo '<p class="form-control-static">'.$txt.'</p>'; ?>
+									  <?php	$txt=$this->qid[$visible[0]];
+									  		if(isset($visible[1])){$txt.=' ['.$this->eqRS[$visible[1]].']';}
+									  		if(isset($visible[2])){$txt.=' [Option #'.$visible[2].']';}
+											echo '<p class="form-control-static visible-p">'.$txt.'</p>'; ?>
 										</div>
 									</div>
 								</form>
@@ -201,12 +197,6 @@ class edit_survey extends template
       				<div class="modal-body">
 						<form class="form-horizontal">
 							<div class="form-group">
-								<label class="control-label col-sm-2" for="add_question_page">Page No:</label>
-								<div class="col-sm-10">
-									<input type="text" class="form-control" id="add_question_page">
-								</div>
-							</div>
-							<div class="form-group">
 								<label class="control-label col-sm-2" for="add_question_no">Question No:</label>
 								<div class="col-sm-10">
 									<input type="text" class="form-control" id="add_question_no">
@@ -229,7 +219,7 @@ class edit_survey extends template
 									</select>
 								</div>
 							</div>
-							<div class="visible_options">
+							<div class="visible_options" style="display:none">
 								<div class="form-group">
 									<label class="control-label col-sm-2">Option #1:</label>
 									<div class="col-sm-10">
@@ -255,68 +245,44 @@ class edit_survey extends template
 									</div>
 								</div>
 							</div>
-
-									<!--<div style="margin-bottom:3px;margin-top:3px">
-										<a class="add-option" data-toggle="modal"><i class="fa fa-plus fa-lg"></i> Add Option</a>
-									</div>-->
-								
 							<div class="form-group">
-								<label class="control-label col-sm-2" for="is_required">Required?:</label>
-								<div class="col-sm-2">
-									<select class="form-control" id="add_is_required">
-							 	  <?php	foreach($this->ynRS as $key2=>$val2){
-											echo '<option value="'.$key2.'">'.$val2.'</option>';
-										} ?>
-									</select>
-								</div>
+								<label class="control-label col-sm-2" for="add_qrequired">Required?:</label>
 								<div class="col-sm-4 required_conditionA"> 
 									<select class="form-control" id="add_qrequired">
-										<option value="" disabled selected>Select Condition...</option>
-										<option value="q0">Always</option>
-										<option value="q1:1">If Question 1</option>
 									</select>
 								</div>
-								<div class="col-sm-2 required_conditionB"> 
+								<div class="col-sm-3 required_conditionB"> 
 									<select class="form-control" id="add_qrequired_condition">
 										<option value="" disabled selected>Condition Equality...</option>
 										<option value="eq">Equals</option>
 										<option value="neq">Not Equals</option>
 									</select>
 								</div>
-								<div class="col-sm-2 required_conditionB"> 
-									<input type="text" class="form-control" id="add_qrequired_condition" placeholder="Option ID">
+								<div class="col-sm-3 required_conditionB"> 
+									<input type="text" class="form-control" id="add_qrequired_option" placeholder="Option ID" value="">
 								</div>
 							</div>
 							<div class="form-group">
-								<label class="control-label col-sm-2" for="add_is_visible">Visible?:</label>
-								<div class="col-sm-2">
-									<select class="form-control" id="add_is_visible">
-										<option value="Y">Yes</option>
-										<option value="N">No</option>
-									</select>
-								</div>
+								<label class="control-label col-sm-2" for="add_qvisible">Visible?:</label>
 								<div class="col-sm-4 visible_conditionA"> 
 									<select class="form-control" id="add_qvisible">
-										<option value="" disabled selected>Select Condition...</option>
-										<option value="q0">Always</option>
-										<option value="q1:1">If Question 1</option>
 									</select>
 								</div>
-								<div class="col-sm-2 visible_conditionB"> 
+								<div class="col-sm-3 visible_conditionB"> 
 									<select class="form-control" id="add_qvisible_condition">
 										<option value="" disabled selected>Condition Equality...</option>
 										<option value="eq">Equals</option>
 										<option value="neq">Not Equals</option>
 									</select>
 								</div>
-								<div class="col-sm-2 visible_conditionB"> 
-									<input type="text" class="form-control" id="add_qvisible_condition" placeholder="Option ID">
+								<div class="col-sm-3 visible_conditionB"> 
+									<input type="text" class="form-control" id="add_qvisible_option" placeholder="Option ID" value="">
 								</div>
 							</div>
 						</form>
 					</div>
 					<div class="modal-footer">
-						<button type="button" class="btn btn-primary" id="yesDeleteModalOptionModal">Yes</button>
+						<button type="button" class="btn btn-primary" id="saveAddQuestionModal">Save</button>
 						<button type="button" class="btn btn-warning" data-dismiss="modal">Cancel</button>
 					</div>
 				</div>
--- js/edit_survey.js
+++ js/edit_survey.js
@@ -3,51 +3,49 @@ $(document).ready(function(){
    var $qtype=$(".qtype"), $addOptionModal=$("#addOptionModal"), $add_option_text=$("#add-option-text");
    var $editOptionModal=$("#editOptionModal"), $edit_option_text=$("#edit-option-text");
    var $confirmDeleteOptionModal=$("#confirmDeleteOptionModal"), $addQuestionModal=$("#addQuestionModal");
+   var $add_qrequired=$("#add_qrequired"), $add_qvisible=$("#add_qvisible"), $add_question_no=$("#add_question_no", $addQuestionModal);
+   var $add_question=$("#add_question", $addQuestionModal), $add_qtype=$("#add_qtype",$addQuestionModal);
+   var $add_option1=$("#add_option1",$addQuestionModal), $add_option2=$("#add_option2",$addQuestionModal);
+   var $add_option3=$("#add_option3",$addQuestionModal), $add_option4=$("#add_option4",$addQuestionModal);
+   var $add_qrequired_condition=$("#add_qrequired_condition",$addQuestionModal), $add_qvisible_condition=$("#add_qvisible_condition",$addQuestionModal);
+   var $add_qrequired_option=$("#add_qrequired_option",$addQuestionModal), $add_qvisible_option=$("#add_qvisible_option",$addQuestionModal);
 
-   $is_required.on("change",function(){
-      let $panel=$(this).closest('div.panel-body');
+   String.prototype.htmlEncode = function() {
+      return this.replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/\n/g,"<br>");
+   }
+   String.prototype.htmlDecode = function() {
+      return this.replace(/&lt;/g,"<").replace(/&gt;/g,">").replace(/<br>/g,"\n");
+   }
+   
+   function serializeJson($form) { 
+      let retval={};
+      $.each($("input, select, textarea",$form),function(){
+         if($(this).val()===null){retval[$(this).attr("id")]="";}
+         else{retval[$(this).attr("id")]=$(this).val();}
+      });
+      return retval;
+   }
+   
+   $add_qrequired.change(function(){
       switch($(this).val()){
          case 'Y':
-            $(".required_conditionA",$panel).show();
-         break;
          case 'N':
-            $(".required_conditionA",$panel).hide();
-         break;
-      }
-   });
-
-   $qrequired.on("change",function(){
-      let $panel=$(this).closest('div.panel-body');
-      switch($(this).val()){
-         case 'q0':
-            $(".required_conditionB",$panel).hide();
+            $(".required_conditionB",$addQuestionModal).hide();
          break;
          default:
-            $(".required_conditionB",$panel).show();
+            $(".required_conditionB",$addQuestionModal).show();
          break;
       }
    });
 
-   $is_visible.on("change",function(){
-      let $panel=$(this).closest('div.panel-body');
+   $add_qvisible.change(function(){
       switch($(this).val()){
          case 'Y':
-            $(".visible_conditionA",$panel).show();
-         break;
          case 'N':
-            $(".visible_conditionA",$panel).hide();
-         break;
-      }
-   });
-
-   $qvisible.on("change",function(){
-      let $panel=$(this).closest('div.panel-body');
-      switch($(this).val()){
-         case 'q0':
-            $(".visible_conditionB",$panel).hide();
+            $(".visible_conditionB",$addQuestionModal).hide();
          break;
          default:
-            $(".visible_conditionB",$panel).show();
+            $(".visible_conditionB",$addQuestionModal).show();
          break;
       }
    });
@@ -66,64 +64,58 @@ $(document).ready(function(){
       }
    });
 
-   $(".add-option").click(function(){
-      let $panel=$(this).closest('div.panel-body');
-      $add_option_text.val("");
-      $addOptionModal.data({"panel":$panel}).modal('show');
-   });
-
-
-   $("ul.list-group").on("mouseenter","li.list-group-item",function(){
-      let $this=$(this);
-      if(!$this.hasClass("empty-list")){
-         $(this).addClass("list-group-item-hover");
-      }
-   }).on("mouseleave","li.list-group-item",function(){
-      $(this).removeClass("list-group-item-hover");
-   }).on("click","li.list-group-item",function(){
-      let $this=$(this);
-      if(!$this.hasClass("empty-list")){
-         $edit_option_text.val("");
-         let option_value=$this.html().replace(/&lt;/g,"<").replace(/&gt;/g,">").replace(/<br>/g,"\n");
-         $edit_option_text.val(option_value);
-         $editOptionModal.data({"list-item":$this}).modal('show');
-      }
-   });
-
-   $("#saveAddOptionModal").click(function(){
-      let $panel=$addOptionModal.data("panel");
-      let $list_group=$("ul.list-group",$panel);
-      let option_value=$add_option_text.val().replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/\n/g,"<br>");
-      $("li.empty-list", $list_group).remove();
-      let $li=$("<li></li>").addClass("list-group-item").append(option_value);
-      $list_group.append($li);
-      $addOptionModal.modal('hide');
-      return false;
-   });
-
-   $("#saveEditOptionModal").click(function(){
-      let $list_item=$editOptionModal.data("list-item");
-      let option_value=$edit_option_text.val().replace(/</g,"&lt;").replace(/>/g,"&gt;").replace(/\n/g,"<br>");
-      $list_item.html(option_value);
-      $editOptionModal.modal('hide');
-      return false;
-   });
-   
-   $("#deleteEditOptionModal").click(function(){
-      let $list_item=$editOptionModal.data("list-item");
-      $("#deleteOptionText").html($list_item.html().replace(/\n/g,"<br>"));
-      $confirmDeleteOptionModal.modal('show').data({"list-item":$editOptionModal.data("list-item")});
-      return false;
-   });
-
-   $("#yesDeleteModalOptionModal").click(function(){
-      let $list_item=$editOptionModal.data("list-item");
-      $list_item.remove();
-      $editOptionModal.modal('hide');
-      $confirmDeleteOptionModal.modal('hide');
-   });
-
    $("#add_question").click(function(){
+      $("input,select,textarea", $addQuestionModal).val("");
+      let $options='<option value="Y">Yes</option><option value="N">No</option>';
+      $.each($("#questionsDiv div.panel"),function(){
+         if($(this).data("has-options")=='Y'){
+            $options+='<option value="q'+$(this).data("pkey")+'">Yes, If Question (ID='+$(this).data("pkey")+')</option>';
+         }
+      });
+      $add_qrequired.html($options);
+      $add_qvisible.html($options);
+      $(".required_conditionB, .visible_conditionB",$addQuestionModal).hide();
       $addQuestionModal.modal('show');
+   });
+
+   $("#saveAddQuestionModal").click(function(){
+      let $qDiv=$("#questionsDiv div.row:eq(0)").clone();
+      $(".question-number-p", $qDiv).html($add_question_no.val());
+      $(".question-p", $qDiv).html($add_question.val().htmlEncode());
+      $(".question-type-p", $qDiv).html($("option:selected",$add_qtype).text());
+      switch($add_qtype.val()){
+         case 'radio':
+         case 'checkbox':
+         case 'dropdown':
+            $(".form-horizontal .form-group:eq(3)", $qDiv).css("display","block");
+            let $ul_group=$("ul.list-group", $qDiv);
+            $ul_group.html("");
+            if($add_option1.val()!=""){$ul_group.append('<li class="list-group-item">'+$add_option1.val().htmlEncode()+'</li>');}
+            if($add_option2.val()!=""){$ul_group.append('<li class="list-group-item">'+$add_option2.val().htmlEncode()+'</li>');}
+            if($add_option3.val()!=""){$ul_group.append('<li class="list-group-item">'+$add_option3.val().htmlEncode()+'</li>');}
+            if($add_option4.val()!=""){$ul_group.append('<li class="list-group-item">'+$add_option4.val().htmlEncode()+'</li>');}
+         break;
+         default:
+            $(".form-horizontal .form-group:eq(3)", $qDiv).css("display","none");
+         break;
+      }
+      let req=$("option:selected",$add_qrequired).text();
+      if($add_qrequired_condition.val()!=''&&$add_qrequired_condition.val()!=null){req+=' ['+$("option:selected",$add_qrequired_condition).text()+']';}
+      if($add_qrequired_option.val()!=""){req+=' [Option #'+$add_qrequired_option.val()+']';}
+      $(".required-p", $qDiv).html(req);
+
+      let vis=$("option:selected",$add_qvisible).text();
+      if($add_qvisible_condition.val()!=''&&$add_qvisible_condition.val()!=null){vis+=' ['+$("option:selected",$add_qvisible_condition).text()+']';}
+      if($add_qvisible_option.val()!=""){vis+=' [Option #'+$add_qvisible_option.val()+']';}
+      $(".visible-p", $qDiv).html(vis);
+
+      $("#questionsDiv").prepend($qDiv);
+      $addQuestionModal.modal('hide');
+
+      console.log(serializeJson($addQuestionModal));
+      //$.each($("input, select, textarea",$addQuestionModal),function(){
+      //   console.log($(this).attr("id")+" "+$(this).val());
+      //});
+      
    });
 })
